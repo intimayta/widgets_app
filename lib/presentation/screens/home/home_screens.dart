cc158308d3fba51abe42222d7e466dfea40815fc
Intimayta Llanque Roberto P <rintimayta@ine.gob.bo>
2023-05-25 11:41:06 -0400

    go router configurado

--- lib/presentation/screens/home/home_screens.dart
+++ lib/presentation/screens/home/home_screens.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-
-import '../../../config/menu/menu_items.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -60,7 +59,9 @@ class _CustomListTile extends StatelessWidget {
           ),
         ); */
 
-        Navigator.pushNamed(context, menuItem.link);
+        //Navigator.pushNamed(context, menuItem.link);
+
+        context.push(menuItem.link);
       },
     );
   }
