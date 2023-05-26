11174af455995ac22280f968d8086d9360d5ac0a
Intimayta Llanque Roberto P <rintimayta@ine.gob.bo>
2023-05-26 12:12:25 -0400

    screens creadas

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScreen extends StatelessWidget {
+  static const name = 'infinite_screen';
+
+  const InfiniteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
