79d5a2142a62a9f0a5361cb52663700f9ff63c53
Intimayta Llanque Roberto P <rintimayta@ine.gob.bo>
2023-05-29 15:59:12 -0400

    screen terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,4 +39,9 @@ const appMenuItems = <MenuItem>[
       subTitle: 'Stateful widget animado',
       link: '/animated',
       icon: Icons.check_box_outline_blank_rounded),
+  MenuItem(
+      title: 'UI controls',
+      subTitle: 'Serie de controles',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined),
 ];
